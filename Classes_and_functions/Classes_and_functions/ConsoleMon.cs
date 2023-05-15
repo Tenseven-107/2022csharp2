734b51df04e10eead6de282a2701011246c4478d
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-05-15 10:10:53 +0200

    Initial commit

---
+++ Classes_and_functions/Classes_and_functions/ConsoleMon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_functions
+{
+    internal class ConsoleMon
+    {
+        internal int health;
+        internal int energy;
+        internal string name;
+
+        internal void TakeDamage(int damage)
+        {
+            this.health -= damage;
+        }
+
+        internal void DepleteEnergy(int energy)
+        {
+            this.energy -= energy;
+        }
+    }
+
+    internal class Skill
+    {
+        internal int damage;
+        internal int energyCost;
+        internal int name;
+    }
+}
---
+++ Classes_and_functions/Classes_and_functions/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Classes_and_functions
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TestConsoleMonFunctions();
+        }
+
+        static void TestConsoleMonFunctions()
+        {
+            Console.WriteLine("TestConsoleMonFunctions");
+            ConsoleMon mon = new ConsoleMon();
+            mon.TakeDamage(100);
+            mon.DepleteEnergy(20);
+
+            Console.WriteLine(mon.health == -100);
+
+            Console.WriteLine(mon.energy == -20);
+        }
+    }
+}
