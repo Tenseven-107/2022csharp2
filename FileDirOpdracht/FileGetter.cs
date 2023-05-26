a88ea7d892c2fdd08d1a7bcde8b1dcbe2d8c1d4b
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-05-26 15:49:47 +0200

    Did the file directory assignment

---
+++ FileDirOpdracht/FileGetter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDirOpdracht
+{
+    internal class FileGetter
+    {
+        internal DirectoryInfo directory;
+        internal FileInfo[] files;
+        internal DirectoryInfo[] dirs;
+
+        
+        internal FileGetter(string path)
+        {
+            if (path != string.Empty) directory = new DirectoryInfo(path);
+            else directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            
+            files = directory.GetFiles();
+            dirs = directory.GetDirectories();
+        }
+    }
+}
---
+++ FileDirOpdracht/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, here are your requested files: ");
+            Console.WriteLine("");
+
+            gettingFiles("C:\\Users\\nadri\\Development\\School\\proofmap\\Periode_1.3-4\\Csharp\\2022csharp2");
+        }
+
+        static void gettingFiles(string path)
+        {
+            FileGetter getter = new FileGetter(path);
+
+            foreach (FileInfo file in getter.files)
+            {
+                Console.WriteLine(file.Name);
+            }
+
+            foreach (DirectoryInfo dir in getter.dirs)
+            {
+                Console.WriteLine(dir.Name);
+            }
+        }
+    }
+}
