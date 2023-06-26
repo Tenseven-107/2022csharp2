7430d0f3ac088fc3d88fff43a095aaad29e09ae4
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-06-26 12:28:26 +0200

    fixes

--- FileIOOpdracht/Program.cs
+++ FileIOOpdracht/Program.cs
@@ -4,17 +4,25 @@
     {
         static void Main(string[] args)
         {
+            File.WriteAllText("andthis.txt", "hello");
+
             string[] content = File.ReadAllLines("readthis.txt");
             ReadLines(content);
 
 
-            if (Directory.Exists("output")) Directory.CreateDirectory("output");
-            if (File.Exists("output\andthis.txt") == false && Directory.Exists("output") == true) Directory.Move("andthis.txt", "output\andthis.txt");
+            if (Directory.Exists("output") == false)
+            {
+                Directory.CreateDirectory("output");
+            }
+            if (File.Exists("output/andthis.txt") == false && Directory.Exists("output") == true)
+            {
+                File.Move("andthis.txt", "output/andthis.txt");
+            }
 
-            string[] contentTheSecond = File.ReadAllLines("output\andthis.txt");
+            string[] contentTheSecond = File.ReadAllLines("output/andthis.txt");
             ReadLines(contentTheSecond);
 
-            File.AppendAllText("output\andthis.txt", "[HOW WAS YOUR DAY?]");
+            File.AppendAllText("output/andthis.txt", "[HOW WAS YOUR DAY?]");
             ReadLines(contentTheSecond);
         }
 
